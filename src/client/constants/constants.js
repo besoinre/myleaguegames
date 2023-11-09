09c11276f150f6af4b1a7ada9787660af0b33f25
Rémi Besoin <besoinremi@gmail.com>
2023-11-09 13:26:07 +0100

    champion icon fix

--- src/client/constants/constants.js
+++ src/client/constants/constants.js
@@ -2,8 +2,20 @@ export const runesIconLink = (rune) =>
 ("https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/"
     + rune.toLowerCase())
 
-export const championIconLink = (champion) =>
-    ("http://ddragon.leagueoflegends.com/cdn/13.19.1/img/champion/" + champion + ".png")
+export const championIconLink = (champion) => {
+    const championNameReplacement = {
+        Nunuwillump: "Nunu",
+        Monkeyking: "MonkeyKing",
+        Renataglasc: "Renata"
+    }
+
+    champion = champion.replace(/[&'\s]/g, '').toLowerCase()
+    champion = champion.charAt(0).toUpperCase() + champion.slice(1).toLowerCase()
+    if (championNameReplacement.hasOwnProperty(champion))
+        champion = championNameReplacement[champion]
+    return "http://ddragon.leagueoflegends.com/cdn/13.19.1/img/champion/" + champion + ".png"
+}
+
 
 export const summonerSpellIconLink = (spell) =>
     ("http://ddragon.leagueoflegends.com/cdn/13.19.1/img/spell/" + spell + ".png")
